ba13565e06a0761d6c7065f421711ca701dc1317
DeathStorm <ChristianT@OnlyEntertainment.de>
2013-07-31 16:10:16 +0200

    HQ und CT GameProps

--- Assets/Scripts/Globals/GameProperties_CT.cs
+++ Assets/Scripts/Globals/GameProperties_CT.cs
@@ -3,21 +3,38 @@ using System.Collections;
 using System.Collections.Generic;
 using System;
 
-public class GameProperties_CT: GameProperties{
+public class GameProperties_CT : GameProperties
+{
+
+    private static List<string> messages = new List<string>();
 
     public enum FRACTION { USFederation, EuropeCoaliation, AsianIndustryForce };
     public enum VEHICLES { Jeep, LightTank, MediumTank, HeavyTank, Helicopter, FighterJet, Bomber };
+
+    public static GameObject g;
+    public Dictionary<VEHICLES, GameObject> unitPrefabs = new Dictionary<VEHICLES, GameObject>() 
+    {
+        {VEHICLES.Jeep,null},
+        {VEHICLES.LightTank,null},
+        {VEHICLES.MediumTank,null},
+        {VEHICLES.HeavyTank,null},
+        {VEHICLES.Helicopter,null},
+        {VEHICLES.FighterJet,null},
+        {VEHICLES.Bomber,null}
+    };
+
 
     public struct WorkingUnit
     {
         public string name;
         public string toolTip;
         public int unitNumber;
-        public float cost;
+        public int cost;
         public float workingSpeed;
+        public GameObject prefab;
     }
 
-    public Dictionary<VEHICLES, WorkingUnit> workingUnits;
+    public static Dictionary<VEHICLES, WorkingUnit> workingUnits;
 
     public static int credits = 2000;
 
@@ -26,101 +43,107 @@ public class GameProperties_CT: GameProperties{
 
     public static FRACTION fraction = FRACTION.EuropeCoaliation;
 
-	// Use this for initialization
-	private void Start () 
+    // Use this for initialization
+    private void Start()
     {
-	
-	}
+
+    }
 
     private void Awake()
     {
         ConfigureUnitsWorking();
     }
 
-	// Update is called once per frame
+    // Update is called once per frame
     private void Update()
     {
-        creditsTempOverflow += creditsPerTick*Time.deltaTime;
+        creditsTempOverflow += creditsPerTick * Time.deltaTime;
         if (creditsTempOverflow >= creditsPerTick)
         {
             creditsTempOverflow -= creditsPerTick;
             credits += Convert.ToInt32(creditsPerTick);
         }
-	}
+    }
 
-
+    public static void AddMessage(string text)
+    {
+        messages.Add(text);
+    }
+    
 
 
     private void ConfigureUnitsWorking()
     {
-        for (int i = 0; i <=  Enum.GetNames(typeof(VEHICLES)).Length; i++)
+        for (int i = 0; i <= Enum.GetNames(typeof(VEHICLES)).Length; i++)
         {
             WorkingUnit unit = new WorkingUnit();
             switch (i)
             {
                 case (int)VEHICLES.Jeep:
-                    unit.unitNumber = i;
                     unit.name = "Jeep";
                     unit.toolTip = "Dies ist ein Jeep";
-                    unit.cost = 1000.0f;
+                    unit.cost = 1000;
                     unit.workingSpeed = 10.0f;
                     break;
                 case (int)VEHICLES.LightTank:
-                    unit.unitNumber = i;
+                    //unit.unitNumber = i;
                     unit.name = "Leichter Panzer";
                     unit.toolTip = "Dies ist ein leichter Panzer";
-                    unit.cost = 1000.0f;
+                    unit.cost = 1000;
                     unit.workingSpeed = 10.0f;
                     break;
                 case (int)VEHICLES.MediumTank:
-                    unit.unitNumber = i;
+                    //unit.unitNumber = i;
                     unit.name = "Mittlerer Panzer";
                     unit.toolTip = "Dies ist ein mittlerer Panzer";
-                    unit.cost = 1000.0f;
+                    unit.cost = 1000;
                     unit.workingSpeed = 10.0f;
                     break;
                 case (int)VEHICLES.HeavyTank:
-                    unit.unitNumber = i;
+                    //unit.unitNumber = i;
                     unit.name = "Schwerer Panzer";
                     unit.toolTip = "Dies ist ein dicker fetter mächtiger Panzer";
-                    unit.cost = 1000.0f;
+                    unit.cost = 1000;
                     unit.workingSpeed = 10.0f;
                     break;
                 case (int)VEHICLES.Helicopter:
-                    unit.unitNumber = i;
+                    //unit.unitNumber = i;
                     unit.name = "Helicopter";
                     unit.toolTip = "Dies ist ein Helischrauber oder Hubcopter";
-                    unit.cost = 1000.0f;
+                    unit.cost = 1000;
                     unit.workingSpeed = 10.0f;
                     break;
                 case (int)VEHICLES.FighterJet:
-                    unit.unitNumber = i;
+                    //unit.unitNumber = i;
                     unit.name = "Jäger";
                     unit.toolTip = "Dies ist ein Jäger, der der fliegt nicht der am Boden";
-                    unit.cost = 1000.0f;
+                    unit.cost = 1000;
                     unit.workingSpeed = 10.0f;
                     break;
                 case (int)VEHICLES.Bomber:
-                    unit.unitNumber = i;
+                    //unit.unitNumber = i;
                     unit.name = "Bomber";
                     unit.toolTip = "Dies ist ein Bomber und macht Boom";
-                    unit.cost = 1000.0f;
+                    unit.cost = 1000;
                     unit.workingSpeed = 10.0f;
                     break;
                 default:
-                    unit.unitNumber = i;
+                    //unit.unitNumber = i;
                     unit.name = "-Unknown-";
                     unit.toolTip = "-Unknown-";
-                    unit.cost = 999999.0f;
+                    unit.cost = 999999999;
                     unit.workingSpeed = 0.1f;
-                    break;
+                    break;                    
             }
+            unit.unitNumber = i;
+            unit.prefab = unitPrefabs[(VEHICLES)i];
+
             workingUnits.Add((VEHICLES)i, unit);
         }
-        
-      
 
-       
+
+
+
     }
 
 }
--- Assets/Scripts/Headquarter/HeadQuarter.cs
+++ Assets/Scripts/Headquarter/HeadQuarter.cs
@@ -4,115 +4,186 @@ using System.Linq;
 using System.Text;
 using UnityEngine;
 
-    class HeadQuarter :  MonoBehaviour
+class HeadQuarter : MonoBehaviour
+{
+
+    public bool isActive = false;
+
+    private Color initColor;
+    private Color selectedColor = Color.cyan;
+    private Color mouseOverColor = Color.blue;
+
+    //Letzter Einheitensammelpunkt
+    private RaycastHit hit_MouseCursor; //Informationsspeicher der Raycasts
+    public Vector3 collectionPoint = Vector3.zero;
+
+    //Einheitenbau
+    public const int unitWorkingLineMax = 5;
+    //public bool isUnitWorking = false;
+    //public float unitWorkingSpeed = 10;
+    //public int unitWorkingOn = 9;
+    public float unitWorkingProcess = 0;
+
+    public int unitWorkingLine = 0;
+
+    private GameProperties_CT.WorkingUnit unitWorkingOn = new GameProperties_CT.WorkingUnit();
+
+    public GameObject[] Prefabs = new GameObject[Enum.GetNames(typeof(GameProperties.VEHICLES)).Length];
+
+
+    void Awake()
+    {
+        collectionPoint = gameObject.transform.position;
+        initColor = gameObject.renderer.material.color;
+    }
+
+    public void DeleteFromProductionLine(GameProperties_CT.WorkingUnit unit)
+    {
+        if (unitWorkingLine > 0 &&
+            unit.unitNumber == unitWorkingOn.unitNumber
+            )
+        {
+            unitWorkingLine--;
+            if (unitWorkingLine == 0) unitWorkingProcess = 0;
+            GameProperties.credits += unitWorkingOn.cost;
+
+        }
+    }
+
+    //public void ProduceUnit(int unitWorkingOn, float unitWorkingSpeed)
+    public void ProduceUnit(GameProperties_CT.WorkingUnit unit)
+    {
+        if (this.unitWorkingLine == 0 &&
+            HasEnoughMoney(unit))
+        {
+
+            this.unitWorkingOn = unit;
+            this.unitWorkingLine++;
+            this.unitWorkingProcess = 0;
+            GameProperties.credits -= unitWorkingOn.cost;
+            //this.unitWorkingOn = unitWorkingOn;
+            //this.unitWorkingSpeed = unitWorkingSpeed;
+
+        }
+        //else if (unitWorkingLine > 0 && unitWorkingOn == this.unitWorkingOn && unitWorkingLine < unitWorkingLineMax)
+        else if (unitWorkingLine > 0 &&
+            unitWorkingOn.unitNumber == unit.unitNumber)
+        {
+            if (unitWorkingLine >= unitWorkingLineMax)
+            { GameProperties_CT.AddMessage("Maximale Warteschlange erreicht."); }
+            else if (HasEnoughMoney(unit))
+            { 
+                this.unitWorkingLine++;
+                GameProperties.credits -= unitWorkingOn.cost;
+            }
+        }
+    }
+
+    private bool HasEnoughMoney(GameProperties_CT.WorkingUnit unit)
+    {
+        if (GameProperties.credits >= unit.cost)
+        {
+            return true;
+        }
+        else
+        {
+            GameProperties_CT.AddMessage("Du hast nicht genug Geld (" + unit.cost.ToString() + ")");
+            return false;
+        }
+    }
+
+    void Production()
+    {
+        if (unitWorkingLine > 0)
+        {
+            //unitWorkingProcess += unitWorkingSpeed * Time.deltaTime;
+            unitWorkingProcess += unitWorkingOn.workingSpeed * Time.deltaTime;
+            if (unitWorkingProcess >= 100.0f) //Einheit fertig gebaut
+            {
+                GameObject go = (GameObject)Instantiate(
+                    unitWorkingOn.prefab,
+                    gameObject.transform.position,
+                    gameObject.transform.rotation);
+
+                //GameObject go = (GameObject)Instantiate(
+                //    GameProperties_CT.workingUnits[(GameProperties_CT.VEHICLES)unitWorkingOn].prefab, 
+                //    gameObject.transform.position, 
+                //    gameObject.transform.rotation);
+
+                go.transform.localScale = new Vector3(2, 2, 2);
+                Unit goUnit = go.GetComponent<Unit>();
+                goUnit.ownUnit_OnAction = "walk";
+                goUnit.Unit_GoToPoint(collectionPoint);
+                unitWorkingLine--;
+                unitWorkingProcess = 0;
+            }
+        }
+    }
+
+    void Update()
     {
 
-        public bool isActive = false;
+        //Farbe anpassen wenn HQ Aktiv
+        if (isActive) { gameObject.renderer.material.color = selectedColor; }
 
-        private Color initColor;
-        private Color selectedColor = Color.cyan;
-        private Color mouseOverColor = Color.blue;
-
-        //Letzter Einheitensammelpunkt
-        private RaycastHit hit_MouseCursor; //Informationsspeicher der Raycasts
-        public Vector3 collectionPoint = Vector3.zero;
-
-        //Einheitenbau
-        public const int untiWorkingLineMax = 5;
-        public bool isUnitWorking = false;
-        public int unitWorkingOn = 9;
-        public float unitWorkingProcess = 0;
-        public float unitWorkingSpeed = 10;
-        public int unitWorkingLine = 0;
-
-        public GameObject[] Prefabs = new GameObject[Enum.GetNames(typeof(GameProperties.VEHICLES)).Length];
-
-
-        void Awake()
+        //Wenn Aktiv und Rechtsklick gemacht wird, dann wird neuer Sammeölpunkt gesetzt.
+        if (isActive && Input.GetMouseButtonDown(1) && Physics.Raycast(Camera.mainCamera.ScreenPointToRay(Input.mousePosition), out hit_MouseCursor, Mathf.Infinity, 512/*9: Weg collider*/))
         {
-            collectionPoint = gameObject.transform.position;
-            initColor = gameObject.renderer.material.color;
+            collectionPoint = hit_MouseCursor.point;
         }
 
-        public void ProduceUnit(int unitWorkingOn, float unitWorkingSpeed)
+        Production();
+                    
+        ////Eigentliche Produktion der Einheiten
+        //if (isUnitWorking)
+        //{
+        //    unitWorkingProcess += unitWorkingSpeed * Time.deltaTime;
+        //    if (unitWorkingProcess >= 100)
+        //    {
+        //        if (Prefabs[unitWorkingOn - 1] != null)
+        //        {
+        //            GameObject go = (GameObject)Instantiate(Prefabs[unitWorkingOn - 1], gameObject.transform.position, gameObject.transform.rotation);
+        //            go.transform.localScale = new Vector3(2, 2, 2);
+        //            Unit goUnit = go.GetComponent<Unit>();
+        //            goUnit.ownUnit_OnAction = "walk";
+        //            goUnit.Unit_GoToPoint(collectionPoint);
+        //        }
+
+        //        isUnitWorking = false;
+        //        unitWorkingOn = 9;
+        //        unitWorkingProcess = 0;
+
+
+        //    }
+        //}   
+    }
+
+    private void OnMouseEnter()
+    {
+        gameObject.renderer.material.color = mouseOverColor;
+    }
+
+    private void OnMouseExit()
+    {
+        gameObject.renderer.material.color = initColor;
+    }
+
+    private void OnMouseDown()
+    {
+        if (!isActive)
         {
-            if (this.unitWorkingLine == 0)
-            {
-                this.unitWorkingLine++;
-                this.unitWorkingOn = unitWorkingOn;
-                this.unitWorkingSpeed = unitWorkingSpeed;
-                this.unitWorkingProcess = 0;
-            }
-            else if (unitWorkingLine > 0 && unitWorkingOn == this.unitWorkingOn && untiWorkingLine < uni)
-            {
-                this.unitWorkingLine++;
-            }
+            Global_Terrain.DeselectAll();
+            isActive = true;
         }
+    }
 
-
-
-        void Update()
+    public void SetInActive()
+    {
+        if (isActive)
         {
-        
-            //Farbe anpassen wenn HQ Aktiv
-            if (isActive) {gameObject.renderer.material.color = selectedColor;}
-        
-            //Wenn Aktiv und Rechtsklick gemacht wird, dann wird neuer Sammeölpunkt gesetzt.
-            if (isActive && Input.GetMouseButtonDown(1) && Physics.Raycast(Camera.mainCamera.ScreenPointToRay(Input.mousePosition), out hit_MouseCursor, Mathf.Infinity, 512/*9: Weg collider*/))
-            {
-                collectionPoint = hit_MouseCursor.point;
-            }
-
-            //Eigentliche Produktion der Einheiten
-            if (isUnitWorking)
-            {
-                unitWorkingProcess += unitWorkingSpeed * Time.deltaTime;
-                if (unitWorkingProcess >= 100)
-                {
-                    if (Prefabs[unitWorkingOn - 1] != null)
-                    {
-                        GameObject go = (GameObject)Instantiate(Prefabs[unitWorkingOn - 1], gameObject.transform.position, gameObject.transform.rotation);
-                        go.transform.localScale = new Vector3(2, 2, 2);
-                        Unit goUnit = go.GetComponent<Unit>();
-                        goUnit.ownUnit_OnAction = "walk";
-                        goUnit.Unit_GoToPoint(collectionPoint);
-                    }
-
-                    isUnitWorking = false;
-                    unitWorkingOn = 9;
-                    unitWorkingProcess = 0;
-
-
-                }
-            }   
-        }
-
-        private void OnMouseEnter()
-        {
-            gameObject.renderer.material.color = mouseOverColor;
-        }
-
-        private void OnMouseExit()
-        {
+            isActive = false;
             gameObject.renderer.material.color = initColor;
         }
-
-        private void OnMouseDown()
-        {
-            if (!isActive)
-            {                
-                Global_Terrain.DeselectAll();
-                isActive = true;
-            }            
-        }
-
-        public void SetInActive()
-        {
-            if (isActive)
-            {
-                isActive = false;
-                gameObject.renderer.material.color = initColor;
-            }
-        }
-
     }
+
+}
